7b33686e857afef904959fdc4318b69f3221098c
Alexander Zhukov <al.zhukoff@gmail.com>
2013-07-10 00:18:29 +0700

    extended player directive

--- js/player.js
+++ js/player.js
@@ -13,22 +13,38 @@ wwsplayer.directive('player', function(AudioService){
     scope: {
       'src': '=source'
     },
-    link: function(scope, element, attributes){
-
-      scope.player = AudioService;
+    template: '<div ng-transclude><button ng-click="player.playPause()">play/pause</button>' +
+      '<button ng-click="player.reset()">stop</button>' +
+      '<div>position: {{position}}</div>' +
+      '<div>duration: {{duration}}</div></div>',
+    replace: true,
+    transclude: true,
+    controller: function($scope, $element, $attrs, $transclude) {
+      $scope.player = AudioService;
 
       //example of event binding
-      scope.player.on('timeupdate',function(time, duration){
-        scope.$apply(function(){
-          scope.position = time;
-          scope.duration = duration;
+      $scope.player.on('timeupdate',function(time, duration){
+        $scope.$apply(function(){
+          $scope.position = time;
+          $scope.duration = duration;
         });
       });
 
-      scope.$watch('src', function(new_value, old_value){
+      $scope.$watch('src', function(new_value, old_value){
+        console.log('nv: ', new_value);
         //Load the song, every event, class method and Instance attribute from audio5js are accessible from the template
-        scope.player.load(new_value);
+        $scope.player.load(new_value);
       });
+    },
+    compile: function compile(tElement, tAttrs, transclude) {
+      return {
+        pre: function preLink(scope, iElement, iAttrs, controller) {
+        },
+        post: function postLink(scope, iElement, iAttrs, controller) {
+        }
+      }
+    },
+    link: function(scope, element, attributes){
     }
   }
 });

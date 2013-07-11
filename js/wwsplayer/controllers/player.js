037286397938746f30fac90c119e1626854b6c05
Alexander Zhukov <al.zhukoff@gmail.com>
2013-07-11 22:01:57 +0700

    player with search

--- js/wwsplayer/controllers/player.js
+++ js/wwsplayer/controllers/player.js
@@ -1,8 +1,19 @@
 var wwsplayer = angular.module('wwsPlayer', ['Audio5']);
 
-wwsplayer.controller('playerController', ['$scope', 'AudioService', function($scope, AudioService){
+wwsplayer.controller('playerController', ['$scope', '$timeout', function($scope, $timeout){
+
+  var DELAY = 500, delayer;
+
   $scope.source = "files/1.mp3";
   $scope.next = function(){
     $scope.source = "files/2.mp3";
   };
+
+  $scope.$watch('search', function(nv, ov){
+    if (angular.isUndefined(nv)) return;
+    $timeout.cancel(delayer);
+    delayer = $timeout(function(){
+      $scope.$broadcast('search:start', nv, ov);
+    }, DELAY);
+  });
 }]);
--- js/wwsplayer/controllers/playlist.js
+++ js/wwsplayer/controllers/playlist.js
@@ -41,4 +41,25 @@ wwsplayer.controller('playlistController', ['$scope', function($scope){
       "visible": true
     }
   ];
+
+  var getSearchableFields = function(){
+    var fields = [];
+    for(var i = 0; i < $scope.fields.length; i++){
+      if($scope.fields[i].searchable) fields.push($scope.fields[i]);
+    }
+    return fields;
+  };
+  var filterSongs = function(value){
+    var sf = getSearchableFields();
+    for(var i = 0; i < $scope.songs.length; i++){
+      $scope.songs[i].visible = false;
+      for(var l = 0; l < sf.length; l++){
+        if ($scope.songs[i][sf[l].name].indexOf(value) !== -1) $scope.songs[i].visible = true;
+      }
+    }
+  };
+
+  $scope.$on('search:start', function(event, nv, ov){
+    filterSongs(nv);
+  });
 }]);

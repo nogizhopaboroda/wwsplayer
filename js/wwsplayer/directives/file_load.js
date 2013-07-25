3bd2329f288dea21459c0813055441a56e3d2d7c
Alexander Zhukov <al.zhukoff@gmail.com>
2013-07-26 00:55:36 +0700

    itunes media file parser

--- js/wwsplayer/directives/file_load.js
+++ js/wwsplayer/directives/file_load.js
@@ -3,21 +3,11 @@ wwsplayer.directive('fileLoad', function(){
     restrict: 'EA',
     template: '' +
       '<div class="add_file_block">' +
-        '<span>drop your files here or just click to select</span>' +
+        '<span>drop your files <strong>or iTunesMedia list</strong> here or just click to select</span>' +
         '<input class="add_file_input" type="file" name="files[]" multiple/>' +
       '</div>',
     replace: true,
     transclude: true,
-    /*controller: function($scope, $element, $attrs, $transclude) {
-    },
-    compile: function compile(tElement, tAttrs, transclude) {
-      return {
-        pre: function preLink(scope, iElement, iAttrs, controller) {
-        },
-        post: function postLink(scope, iElement, iAttrs, controller) {
-        }
-      }
-    },*/
     link: function(scope, element, attributes){
 
 
